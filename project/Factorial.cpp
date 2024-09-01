08924c8c69214ba4a1027ab4ccf977d78954e477
Recovw <kelvinow27@gmail.com>
2024-09-01 12:14:25 +0700

    Factorial

---
+++ project/Factorial.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int faktorial(int angka);
+
+int main()
+{
+	int angka;
+	
+	printf("Ini adalah perhitungan faktorial, ketik angka apa saja:\n");
+	printf("Ketika angka:");
+    scanf("%d", &angka);
+    
+	printf("Hasil dari faktorial : %lld", faktorial(angka));
+	
+    
+    return 0;
+}
+
+int faktorial(int angka)
+{
+	int long long produk = 1;
+	
+	for (int i = 1; i <= angka; i++)
+	{
+		produk *= i;
+	}
+	
+	return produk;
+}
